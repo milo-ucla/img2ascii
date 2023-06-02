222218533b2456470f85ed7b5fb2b8fc5894c74b
milok23 <milok23@g.ucla.edu>
2023-06-01 19:33:18 -0700

    updates

--- image2ascii.py
+++ image2ascii.py
@@ -1,9 +1,9 @@
-#! /bin/python3
+#! /bin/env python3
 
 import sys
 import argparse
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFilter
 
 
 def get_image_dimensions(image_path: str):
@@ -12,22 +12,54 @@ def get_image_dimensions(image_path: str):
         return (width, height)
 
 
-def get_pixel_buffer(image_path: str):
+def get_pixel_array(image_path: str):
     with Image.open(image_path) as img:
         pixel_array = np.array(img)
         return pixel_array
 
 
-def scale_image(pixel_array, scale_factor : float):
+def scale_image(pixel_array, scale_factor: float):
     img = Image.fromarray(pixel_array)
     target_width = int(img.width * scale_factor)
-    target_height = int(img.height * scale_factor * 0.7)
+    target_height = int(img.height * scale_factor * 0.4)
     resized_img = img.resize((target_width, target_height))
     resized_array = np.array(resized_img)
     return resized_array
 
-def get_ansii_color_array(pixel_array)-> str:
-    
+
+def apply_image_filters(pixel_array, args):
+    img = Image.fromarray(pixel_array)
+    img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    return np.array(img)
+
+
+def convert_pixel_to_ansi_color_bg(pixel) -> str:
+    """Converts a pixel into an ANSI color escape character.
+    Args:
+      pixel: A pixel represented as a 3-tuple of integers.
+    Returns:
+      An ANSI color escape character.
+    """
+    r, g, b = pixel
+    # Create an ANSI color escape character.
+    ansi_color = f"\033[48;2;{r};{g};{b}m\033[30m"
+    return ansi_color
+
+
+def convert_pixel_to_ansi_color_fg(pixel) -> str:
+    """Converts a pixel into an ANSI color escape character.
+    Args:
+      pixel: A pixel represented as a 3-tuple of integers.
+
+    Returns:
+      An ANSI color escape character.
+    """
+    r, g, b = pixel
+    # Create an ANSI color escape character.
+    ansi_color = f"\033[38;2;{r};{g};{b}m"
+    return ansi_color
+
+
 def convert_to_ascii(pixel_array) -> str:
     # Map luminance to ASCII characters
     ascii_chars = "@%#*+=-:. "
@@ -37,10 +69,8 @@ def convert_to_ascii(pixel_array) -> str:
     img = Image.fromarray(pixel_array)
     img = img.convert("L")
     pixel_array = np.array(img)
-
     # Normalize pixel values
     pixel_array = pixel_array / 255.0
-
     # Convert pixel values to ASCII characters based on luminance
     ascii_string = ""
     for row in pixel_array:
@@ -53,19 +83,69 @@ def convert_to_ascii(pixel_array) -> str:
     return ascii_string
 
 
+def convert_bg_to_ansi(pixel_array) -> list[str]:
+    ansi_array: list[str] = []
+    for row in pixel_array:
+        for pixel in row:
+            ansi_array.append(convert_pixel_to_ansi_color_bg(pixel))
+        ansi_array.append("")
+    return ansi_array
+
+
+def convert_fg_to_ansi(pixel_array) -> list[str]:
+    ansi_array: list[str] = []
+    for row in pixel_array:
+        for pixel in row:
+            ansi_array.append(convert_pixel_to_ansi_color_fg(pixel))
+        ansi_array.append("\033[0m")
+    return ansi_array
+
+
 def main() -> None:
-	parser = argparse.ArgumentParser(description="Image processing script")
-	# Add the image path argument
-	parser.add_argument("image_path", help="path to the image file")
-	# Add the scale option
-	parser.add_argument("-s", "--scale", type=float, help="scale factor to resize the image")
-	# Parse the command-line arguments
-	args = parser.parse_args()
-	scale_factor = args.scale if args.scale else 1.0		
-	# Process the image
-	pixel_buff = get_pixel_buffer(args.image_path)
-	pixel_buff = scale_image(pixel_buff, scale_factor)
-	print(convert_to_ascii(pixel_buff))
+    parser = argparse.ArgumentParser(description="Image processing script")
+    # Add the image path argument
+    parser.add_argument(
+        "image_path", help="path to the image file")
+    # Add the scale option
+    parser.add_argument("-s", "--scale", type=float,
+                        help="scale factor to resize the image")
+    parser.add_argument("-b", "--background-color", action="store_true",
+                        help="add ANSI color to the background", default=False)
+    parser.add_argument("-a", "--foreground-color", action="store_true",
+                        help="add ANSI color to the characters", default=False)
+    parser.add_argument("-o", "--omit-characters", action="store_true",
+                        help="replace all ASCII characters with whitespace", default=False)
+    parser.add_argument("-d", "--edge-detection", action="store_true",
+                    help="use laplace edge detection filter on original image", default=False)
+    parser.add_argument("-e", "--edge-enhance", action="store_true",
+                        help="enhance edges and countours of the image", default=False)
+    # Parse the command-line arguments
+    args = parser.parse_args()
+    scale_factor = args.scale if args.scale else 1.0
+    fg_color = args.foreground_color
+    bg_color = args.background_color
+    omit_ascii = args.omit_characters
+    # Process the image
+    pixel_array = get_pixel_array(args.image_path)
+    pixel_array = apply_image_filters(pixel_array, args)
+    pixel_array = scale_image(pixel_array, scale_factor)
+    ascii_text = convert_to_ascii(pixel_array)
+    if (omit_ascii):
+        ascii_text = ''.join([(' ' if char != '\n' else char)
+                             for char in ascii_text])
+    if (fg_color):
+        ansi_fg = convert_fg_to_ansi(pixel_array)
+    else:
+        ansi_fg = ["" for _ in ascii_text]
+
+    if (bg_color):
+        ansi_bg = convert_bg_to_ansi(pixel_array)
+    else:
+        ansi_bg = ["" for _ in ascii_text]
+    final_ascii_text = ""
+    for i in range(len(ascii_text)):
+        final_ascii_text += f"{ansi_bg[i]}{ansi_fg[i]}{ascii_text[i]}\033[0m"
+    print(final_ascii_text)
 
 
 if __name__ == "__main__":
